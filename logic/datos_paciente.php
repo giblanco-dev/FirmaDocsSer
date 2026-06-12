dd76b4999b932b3a63950742f5d4f431f98f0877
DevIvanB <gabriel.blanco@nacerglobal.onmicrosoft.com>
2026-06-12 13:56:55 -0600

    fix: detalles pase a hosting

--- logic/datos_paciente.php
+++ logic/datos_paciente.php
@@ -7,7 +7,7 @@ function obtener_datos_paciente($mysqli, $id_paciente) {
     $id_paciente = $mysqli->real_escape_string($id_paciente);
     $query = "SELECT id_paciente, concat(nombres,' ',a_paterno,' ', a_materno) nombre_comp, fecha_consetimiento_hgc, url_firma_hgc, 
                         fecha_consetimiento_avisop, url_firma_avisop, fecha_consetimientoinf, url_firma_consent, foto 
-                        FROM ser.paciente where id_paciente = '$id_paciente'";
+                        FROM paciente where id_paciente = '$id_paciente'";
     
     $resultado = $mysqli->query($query);
     if ($resultado && $resultado->num_rows == 1) {
